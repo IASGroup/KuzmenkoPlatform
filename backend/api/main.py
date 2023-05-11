13c377c999dbad5127932c9842b81561ec009d2c
Данила Бондаренко <bda@pkzdrav.ru>
2023-05-11 16:45:35 +0300

    temporary

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,13 +2,26 @@ import pandas as pd
 import uvicorn
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def getdata():
-    data = pd.read_csv('backend/api/data/bondarenko/somedata.csv')
+    data = pd.read_csv('../api/data/bondarenko/somedata.csv')
     return data.to_json()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
--- frontend/src/views/fios/bondarenko/BondarenkoView.vue
+++ frontend/src/views/fios/bondarenko/BondarenkoView.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+let data = ref()
+let headers = ref([{}])
+let rows = ref([])
+
+onMounted(async () => {
+  data = await axios.get('http://localhost:8000')
+  let head = data[0]
+});
 </script>
 
 <template>
