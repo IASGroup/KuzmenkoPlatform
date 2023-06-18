169199bc87b7119efa996f150dd0b6d689d9930d
Данила Бондаренко <bda@pkzdrav.ru>
2023-06-19 01:58:20 +0300

    fix dir

--- backend/api/utils/bondarenko/utils.py
+++ backend/api/utils/bondarenko/utils.py
@@ -1,7 +1,12 @@
+import os
+import pathlib
 from ...data.bondarenko.sqlite.utils import init_database
 from ...routes.bondarenko import parserRouter, websocketsRouter, tasksRouter
 
 def init_app(fastApiApp):
+    path = f'{pathlib.Path(__file__).parent.parent.parent}/data/bondarenko/sqlite/database'
+    if not os.path.exists(path):
+        os.mkdir(path)
     init_database()
     fastApiApp.include_router(parserRouter.router)
     fastApiApp.include_router(websocketsRouter.router)
